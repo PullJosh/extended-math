e4e33e4b3afc57baf9f4f9944d007a1cf5124163
Josh Pullen <JoshPullen27@gmail.com>
2015-06-20 18:54:27 -0400

    Humble beginnings...

---
+++ extendedMath.js
@@ -0,0 +1,30 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
+    };
+    
+    ext.distance = function(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ['r', '%n ^ %n', 'power', 2, 3],
+            ['r', 'distance from x: %n y: %n to x: %n y: %n', 'distance', 0, 0, 30, 40]
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Extended Math', descriptor, ext);
+})({});
